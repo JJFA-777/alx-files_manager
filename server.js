37f7102ac5aa2ca5acd9ee2c757900b41621b844
JJFA-777 <jesunboalade@gmail.com>
2025-07-09 13:58:30 +0000

    Task 3

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 import express from 'express';
-import routes from './routes/index';
+import router from './routes/index.js'; 
 
 const app = express();
+app.use(express.json()); 
+app.use('/', router);
+
 const port = process.env.PORT || 5000;
-
-app.use('/', routes);
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-export default app;
